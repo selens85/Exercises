a922c92b8ff8575013f863d1b77ca137c2e27b23
Artis Vilciņš <artisvilcins@gmail.com>
2022-01-09 10:42:18 +0200

    First dictionary exercises

---
+++ Exercises/Level1/Exercises.Level1.Tests/Dictionary1Tests/DictionaryABTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercises.Level1.Tests.Dictionary1Tests
+{
+    public class DictionaryABTests
+    {
+        private readonly Dictionary1 _dictionary1 = new();
+
+
+    }
+}
---
+++ Exercises/Level1/Exercises.Level1.Tests/Dictionary1Tests/DictionaryBullyTests.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Exercises.Level1.Dictionary1Tests.Tests
+{
+    public class DictionaryBullyTests
+    {
+        private readonly Dictionary1 _dictionary1 = new();
+
+        [Fact]
+        public void Test1()
+        {
+            var example = new Dictionary<string, string>
+            {
+                {"a", "candy"},{"b", "dirt"}
+            };
+
+            var response = _dictionary1.DictionaryBully(example);
+
+            var expected = new Dictionary<string, string>
+            {
+                {"a", ""},{"b", "candy"}
+            };
+
+            Assert.Equal(expected, response);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            var example = new Dictionary<string, string>
+            {
+                {"a", "candy"}
+            };
+
+            var response = _dictionary1.DictionaryBully(example);
+
+            var expected = new Dictionary<string, string>
+            {
+                {"a", ""},{"b", "candy"}
+            };
+
+            Assert.Equal(expected, response);
+        }
+
+        [Fact]
+        public void Test3()
+        {
+            var example = new Dictionary<string, string>
+            {
+                {"a", "candy"},{"b", "carrot"},{"c", "meh"}
+            };
+
+            var response = _dictionary1.DictionaryBully(example);
+
+            var expected = new Dictionary<string, string>
+            {
+                {"a", ""},{"b", "candy"},{"c", "meh"}
+            };
+
+            Assert.Equal(expected, response);
+        }
+
+        [Fact]
+        public void Test4()
+        {
+            var example = new Dictionary<string, string>
+            {
+                {"b", "carrot"}
+            };
+
+            var response = _dictionary1.DictionaryBully(example);
+
+            var expected = new Dictionary<string, string>
+            {
+                {"b", "carrot"}
+            };
+
+            Assert.Equal(expected, response);
+        }
+
+        [Fact]
+        public void Test5()
+        {
+            var example = new Dictionary<string, string>
+            {
+                {"c", "meh"}
+            };
+
+            var response = _dictionary1.DictionaryBully(example);
+
+            var expected = new Dictionary<string, string>
+            {
+                {"c", "meh"}
+            };
+
+            Assert.Equal(expected, response);
+        }
+
+        [Fact]
+        public void Test6()
+        {
+            var example = new Dictionary<string, string>
+            {
+                {"a", "sparkle"},{"c", "meh"}
+            };
+
+            var response = _dictionary1.DictionaryBully(example);
+
+            var expected = new Dictionary<string, string>
+            {
+                {"a", ""},{"b", "sparkle"},{"c", "meh"}
+            };
+
+            Assert.Equal(expected, response);
+        }
+    }
+}
